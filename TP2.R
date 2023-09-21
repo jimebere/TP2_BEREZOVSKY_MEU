df655a520114384442400fe5230ed75c029846cb
jimebere <jimebere@gmail.com>
2023-09-20 23:58:37 -0300

    PRUEBA DE CONEXIÓN

---
+++ TP2.R
@@ -0,0 +1,8 @@
+#INSTRUMENTOS_DE_ANALISIS_URBANO_TP2
+
+#1. Se creó un repositorio en mi cuenta personal de GitHub: jimebere/TP1_MEU, con versión de control. 
+
+#2. Se instalaron los paquetes y librerías
+
+install.packages("tidyverse")
+library(tidyverse)
